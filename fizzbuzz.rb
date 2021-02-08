418f6f5c2007fee487c1c1b8fefe4341b39f0cbc
Kassandra Guerrero <kassandra-g@hotmail.com>
2021-02-08 00:58:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,7 @@
 def fizzbuzz(number)
-  if number % 3 == 0
+if (number % 3 ==0) && (number % 5 == 0)
+  puts "FizzBuzz"
+elseif number % 3 == 0
     puts "Fizz"
   elseif number % 5 == 0
   puts "Buzz"
